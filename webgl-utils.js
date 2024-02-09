c799c96e36d95b826d0da273592e47d3aa3235c8
Elli Beres <ellifteria@gmail.com>
2024-02-09 11:14:08 -0600

    Add shader and program creation

--- webgl-utils.js
+++ webgl-utils.js
@@ -7,6 +7,11 @@ var WebGLUtils = function () {
         WebGLContextType[WebGLContextType["WebGPU"] = 3] = "WebGPU";
         WebGLContextType[WebGLContextType["BitMapRenderer"] = 4] = "BitMapRenderer";
     })(WebGLContextType || (WebGLContextType = {}));
+    var WebGLShaderType;
+    (function (WebGLShaderType) {
+        WebGLShaderType[WebGLShaderType["VertexShader"] = 0] = "VertexShader";
+        WebGLShaderType[WebGLShaderType["FragmentShader"] = 1] = "FragmentShader";
+    })(WebGLShaderType || (WebGLShaderType = {}));
     var setupWebGL = function (canvas, webglContextType, contextAttributes) {
         if (webglContextType === void 0) { webglContextType = WebGLContextType.WebGL; }
         var contextType = (function () {
@@ -26,13 +31,59 @@ var WebGLUtils = function () {
             }
             ;
         })();
-        var context = canvas.getContext(contextType, contextAttributes);
+        var context;
+        if (typeof (contextAttributes) !== 'undefined') {
+            context = canvas.getContext(contextType, contextAttributes);
+        }
+        else {
+            context = canvas.getContext(contextType);
+        }
         return context;
     };
-    var createShader = function () { };
-    var createProgram = function () { };
+    var createShader = function (gl, type, source) {
+        var glShaderType = (function () {
+            switch (type) {
+                case WebGLShaderType.VertexShader:
+                    return gl.VERTEX_SHADER;
+                case WebGLShaderType.FragmentShader:
+                    return gl.FRAGMENT_SHADER;
+            }
+        })();
+        var shader = gl.createShader(glShaderType);
+        if (shader === null)
+            return null;
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+        var compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+        if (!compiled) {
+            var compilationError = gl.getShaderInfoLog(shader);
+            console.log('Failed tp compile shader: ' + compilationError);
+            gl.deleteShader(shader);
+            return null;
+        }
+        return shader;
+    };
+    var createProgram = function (gl, vertexShader, fragmentShader) {
+        var program = gl.createProgram;
+        if (program === null)
+            return null;
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+        var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+        if (!linked) {
+            var linkError = gl.getProgramInfoLog(program);
+            console.log('Failed to link program: ' + linkError);
+            gl.deleteProgram(program);
+            gl.deleteShader(vertexShader);
+            gl.deleteShader(fragmentShader);
+            return null;
+        }
+        return program;
+    };
     return {
         WebGLContextType: WebGLContextType,
+        WebGLShaderType: WebGLShaderType,
         setupWebGL: setupWebGL,
         createShader: createShader,
         createProgram: createProgram,
--- webgl-utils.ts
+++ webgl-utils.ts
@@ -7,7 +7,12 @@ var WebGLUtils = function() {
         BitMapRenderer
     }
 
-    var setupWebGL = function(canvas:  HTMLCanvasElement, webglContextType: WebGLContextType = WebGLContextType.WebGL, contextAttributes: any | null) {
+    enum WebGLShaderType {
+        VertexShader,
+        FragmentShader
+    }
+
+    var setupWebGL = function(canvas:  HTMLCanvasElement, webglContextType: WebGLContextType = WebGLContextType.WebGL, contextAttributes?: Object): RenderingContext | null {
         let contextType: string = (function() {
             switch(webglContextType) {
                 case WebGLContextType.TwoD:
@@ -25,19 +30,72 @@ var WebGLUtils = function() {
             };
         })();
 
-        let context = canvas.getContext(contextType, contextAttributes);
-        
-        return context;
-    }
+        let context: RenderingContext | null;
+        if (typeof(contextAttributes) !== 'undefined') {
+            context = canvas.getContext(contextType, contextAttributes);
+        } else {
+            context = canvas.getContext(contextType);
+        }
 
-    var createShader = function(){};
+        return context;
+    };
+
+    var createShader = function(gl: WebGL2RenderingContext | WebGLRenderingContext, type: WebGLShaderType, source: string): WebGLShader | null {
+        let glShaderType = (function() {
+            switch(type) {
+                case WebGLShaderType.VertexShader:
+                    return gl.VERTEX_SHADER;
+                case WebGLShaderType.FragmentShader:
+                    return gl.FRAGMENT_SHADER;
+            }
+        })();
+
+        let shader: WebGLShader | null = gl.createShader(glShaderType);
+
+        if (shader === null) return null;
+
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+
+        let compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+        if (!compiled) {
+            let compilationError = gl.getShaderInfoLog(shader);
+            console.log('Failed tp compile shader: ' + compilationError);
+            gl.deleteShader(shader);
+            return null;
+        }
+
+        return shader;
+    };
     
-    var createProgram = function(){};
+    var createProgram = function(gl: WebGLRenderingContext | WebGL2RenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader): WebGLProgram | null {
+        let program: WebGLProgram | null = gl.createProgram;
+        if (program === null) return null;
+
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+
+        let linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+
+        if (!linked) {
+            let linkError = gl.getProgramInfoLog(program);
+            console.log('Failed to link program: ' + linkError);
+            gl.deleteProgram(program);
+            gl.deleteShader(vertexShader);
+            gl.deleteShader(fragmentShader);
+            return null;
+        }
+
+        return program;
+    };
 
     return {
         WebGLContextType: WebGLContextType,
+        WebGLShaderType: WebGLShaderType,
         setupWebGL: setupWebGL,
         createShader: createShader,
         createProgram: createProgram,
     };
-};
+}
